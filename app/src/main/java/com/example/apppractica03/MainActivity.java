fc3c6323324fe16640a35ae6f63e390ceaf16119
Carlos rodriguez <2019030420@upsin.edu.mx>
2022-05-12 21:49:48 -0600

    Aplicacion movil utilzando un spinner

---
+++ app/src/main/java/com/example/apppractica03/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.apppractica03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner spnPaises;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        spnPaises = (Spinner) findViewById(R.id.spnPaises);
+        ArrayAdapter<String> adaptador =
+                new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.paises));
+        spnPaises.setAdapter(adaptador);
+        spnPaises.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(!adapterView.getItemAtPosition(i).toString().matches("")){
+                    Toast.makeText(MainActivity.this, "Seleccionó el pais "+adapterView.getItemAtPosition(i).toString(), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+}
